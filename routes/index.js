8b3add1971a7fc1a9854488959bced8102b103a9
Ronan Manoj <ronmantech@gmail.com>
2018-10-20 22:32:57 -0700

    comments update:

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET request to HOME page. */
 router.get('/', (req, res, next) =>
 {
   res.render('index', { title: 'Express' });
 });
 
+/* GET request to LOGIN form page. */
 router.get('/login', (req, res, next) =>
 {
-  res.render('login', { title: 'Study Buddies Login', cssOne: 'loginUtil.css', cssTwo: 'loginMain.css' });
+  res.render('login', { title: 'Study Buddies Login',
+                        cssOne: 'loginUtil.css',
+                        cssTwo: 'loginMain.css'
+                      });
 });
 
 module.exports = router;
