c4c050c7c00077ae15cd2fb95f4478e302226e85
Ronan Manoj <ronmantech@gmail.com>
2018-10-21 03:32:18 -0700

    update, including helmet for security purposes

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
-const indexRouter = require('./routes/index');
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -15,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
